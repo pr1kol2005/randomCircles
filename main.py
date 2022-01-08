8d18b17ce2eb02483bd187832594c1e7222b1e83
Cyril <k.a.grinko@gmail.com>
2022-01-08 16:24:54 +0300

    Third commit

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 import sys
 
-from PyQt5 import uic
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+from PyQt5.QtWidgets import QWidget, QApplication
 from random import randrange
-from UI import Ui_Form
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(1600, 900)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(Form.sizePolicy().hasHeightForWidth())
+        Form.setSizePolicy(sizePolicy)
+        Form.setMinimumSize(QtCore.QSize(1600, 900))
+        Form.setMaximumSize(QtCore.QSize(1600, 900))
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(30, 20, 291, 101))
+        font = QtGui.QFont()
+        font.setPointSize(36)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "PRESS ME"))
 
 
 class RandomCircle(QWidget, Ui_Form):
@@ -30,6 +56,7 @@ class RandomCircle(QWidget, Ui_Form):
         colors = (randrange(0, 255), randrange(0, 255), randrange(0, 255))
         qp.setBrush(QColor(*colors))
         qp.drawEllipse(randrange(0, 1600), randrange(0, 900), d, d)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
